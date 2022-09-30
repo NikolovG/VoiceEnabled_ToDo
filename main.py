61be58043865bae7cb13ad4ccaf9400f5d1c4b43
Georgi Nikolov <Nikolov.georgi18@gmail.com>
2022-09-30 07:45:15 -0500

    audio input enabled

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
+# NOTE: Need the following libraries to make script run
+#       speechRecognizer
+#       pyaudio
+
 import speech_recognition as sr
+import pyaudio
+
+
 print(sr.__version__)
 
 r = sr.Recognizer()
+myMic = sr.Microphone(device_index=0) # may be sensitive to system change
 
-harvard = sr.AudioFile('audio/OSR_us_000_0010_8k.wav')
+
+harvard = sr.AudioFile('testAudio/OSR_us_000_0010_8k.wav')
 with harvard as source:
     audio = r.record(source)
+print(type(audio))
 
-type(audio)
 
-#print(r.recognize_google(audio))
+#transcribe = r.recognize_google(audio)
+#print(type(transcribe))
+#print(transcribe)
 
-transcribe = r.recognize_google(audio)
-print(type(transcribe))
 
+#parsed = transcribe.split() # splits the string into a list of words
+
+print("\n")
+
+with myMic as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+voiceOutput = r.recognize_google(audio)
+print(voiceOutput.split())
+
+for i in voiceOutput.split():
+    print(i)
+
+
+# How will it handle punctuation???
+# NOTE: does not react to questions
